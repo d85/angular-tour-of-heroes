8c74cba8bb3b720f9a6cfe366294c5342f3881db
Dan Carlier <dan.carlier@gmail.com>
2019-04-09 05:36:49 +1000

    Added Hero service and Message component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +8,7 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
   hero: Hero = {
     id: 1,
@@ -20,9 +20,20 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  /* The paramereter simulataneously defines a private heroService property
+  *  and identifies it as a HeroService injection site */
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  /* The new version waits for the Observable to emit the array of heroes -
+  * which could happen now or several minutes from now. Then subscribe passes
+  * the emitted array to the callback, which sets the components heroes property */
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
